73c23b9a550abf0281db882c114bbdcaffdf5f9c
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2022-08-02 12:17:28 +0000

    OP4EA7: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: oppo Model: PDRM00 Script version: 2.2.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_OP4EA7.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_OP4EA7-user \
+    omni_OP4EA7-userdebug \
+    omni_OP4EA7-eng
---
+++ omni_OP4EA7.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from OP4EA7 device
+$(call inherit-product, device/oppo/OP4EA7/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := OP4EA7
+PRODUCT_NAME := omni_OP4EA7
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := PDRM00
+PRODUCT_MANUFACTURER := oppo
